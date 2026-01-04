ddbcc39ef5ae86fe87060676d5a0052c5988f918
coa021 <coa2102001@gmail.com>
2026-01-04 19:17:29 +0100

    style: add .clang-format and apply LLVM style

--- include/core/types.hpp
+++ include/core/types.hpp
@@ -4,178 +4,163 @@
 #include <cstdint>
 #include <span>
 
-namespace chip8::constants
-{
-    inline constexpr std::size_t MEMORY_SIZE{4096};
-    inline constexpr std::size_t PROGRAM_START{0x200};
-    inline constexpr std::size_t FONT_START{0x050};
-    inline constexpr std::size_t STACK_SIZE{16};
+namespace chip8 {
+namespace constants {
+inline constexpr std::size_t MEMORY_SIZE{4096};
+inline constexpr std::size_t PROGRAM_START{0x200};
+inline constexpr std::size_t FONT_START{0x050};
+inline constexpr std::size_t STACK_SIZE{16};
 
-    inline constexpr std::size_t DISPLAY_WIDTH{64};
-    inline constexpr std::size_t DISPLAY_HEIGHT{32};
-    inline constexpr std::size_t DISPLAY_PIXELS{DISPLAY_WIDTH * DISPLAY_HEIGHT};
+inline constexpr std::size_t DISPLAY_WIDTH{64};
+inline constexpr std::size_t DISPLAY_HEIGHT{32};
+inline constexpr std::size_t DISPLAY_PIXELS{DISPLAY_WIDTH * DISPLAY_HEIGHT};
 
-    inline constexpr std::size_t NUM_REGISTERS{16};
-    inline constexpr std::size_t NUM_KEYS{16};
+inline constexpr std::size_t NUM_REGISTERS{16};
+inline constexpr std::size_t NUM_KEYS{16};
 
-    inline constexpr double TIMER_FREQUENCY_HZ{60.0};
-    inline constexpr double DEFAULT_CPU_FREQUENCY_HZ{500.0};
+inline constexpr double TIMER_FREQUENCY_HZ{60.0};
+inline constexpr double DEFAULT_CPU_FREQUENCY_HZ{500.0};
 
-    inline constexpr std::size_t FONT_SPRITE_HEIGHT{5};
-    inline constexpr std::size_t FONT_CHAR_COUNT{16};
+inline constexpr std::size_t FONT_SPRITE_HEIGHT{5};
+inline constexpr std::size_t FONT_CHAR_COUNT{16};
 
-    inline constexpr std::array<uint8_t, FONT_CHAR_COUNT * FONT_SPRITE_HEIGHT> FONT_SET{
-        {
-            {
-                0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
-                0x20, 0x60, 0x20, 0x20, 0x70, // 1
-                0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
-                0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
-                0x90, 0x90, 0xF0, 0x10, 0x10, // 4
-                0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
-                0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
-                0xF0, 0x10, 0x20, 0x40, 0x40, // 7
-                0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
-                0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
-                0xF0, 0x90, 0xF0, 0x90, 0x90, // A
-                0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
-                0xF0, 0x80, 0x80, 0x80, 0xF0, // C
-                0xE0, 0x90, 0x90, 0x90, 0xE0, // D
-                0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-                0xF0, 0x80, 0xF0, 0x80, 0x80 // F
-            }
-        }
-    };
+inline constexpr std::array<uint8_t, FONT_CHAR_COUNT * FONT_SPRITE_HEIGHT>
+FONT_SET{
+    {
+        0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+        0x20, 0x60, 0x20, 0x20, 0x70, // 1
+        0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+        0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+        0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+        0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+        0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+        0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+        0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+        0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+        0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+        0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+        0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+        0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+        0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+        0xF0, 0x80, 0xF0, 0x80, 0x80 // F
+    }
+};
 }
 
 template <typename T, typename Tag>
-class StrongType
-{
+class StrongType {
 public:
-    constexpr StrongType() noexcept : m_Value{}
-    {
-    }
+  constexpr StrongType() noexcept : m_Value{} {
+  }
 
-    constexpr explicit StrongType(T val) noexcept : m_Value{val}
-    {
-    }
+  constexpr explicit StrongType(T val) noexcept : m_Value{val} {
+  }
 
-    [[nodiscard]] constexpr T get() const noexcept { return m_Value; }
-    [[nodiscard]] constexpr explicit operator T() const noexcept { return m_Value; }
+  [[nodiscard]] constexpr T get() const noexcept { return m_Value; }
 
-    [[nodiscard]] constexpr auto operator<=>(const StrongType&) const noexcept = default;
-    [[nodiscard]] constexpr bool operator==(const StrongType&) const noexcept = default;
+  [[nodiscard]] constexpr explicit operator T() const noexcept {
+    return m_Value;
+  }
 
-    // arithmetics
-    constexpr StrongType& operator++() noexcept
-    {
-        ++m_Value;
-        return *this;
-    }
+  [[nodiscard]] constexpr auto operator<=>(const StrongType &) const noexcept
+  = default;
+  [[nodiscard]] constexpr bool operator==(const StrongType &) const noexcept
+  = default;
 
-    constexpr StrongType operator++(int) noexcept
-    {
-        auto tmp = *this;
-        ++m_Value;
-        return tmp;
-    }
+  // arithmetics
+  constexpr StrongType &operator++() noexcept {
+    ++m_Value;
+    return *this;
+  }
 
-    constexpr StrongType& operator--() noexcept
-    {
-        --m_Value;
-        return *this;
-    }
+  constexpr StrongType operator++(int) noexcept {
+    auto tmp = *this;
+    ++m_Value;
+    return tmp;
+  }
 
-    constexpr StrongType operator--(int) noexcept
-    {
-        auto tmp = *this;
-        --m_Value;
-        return tmp;
-    }
+  constexpr StrongType &operator--() noexcept {
+    --m_Value;
+    return *this;
+  }
 
-    [[nodiscard]] constexpr StrongType operator+(const StrongType& other) const noexcept
-    {
-        return StrongType{static_cast<T>(m_Value + other.m_Value)};
-    }
+  constexpr StrongType operator--(int) noexcept {
+    auto tmp = *this;
+    --m_Value;
+    return tmp;
+  }
 
-    [[nodiscard]] constexpr StrongType operator-(const StrongType& other) const noexcept
-    {
-        return StrongType{static_cast<T>(m_Value - other.m_Value)};
-    }
+  [[nodiscard]] constexpr StrongType operator+(
+      const StrongType &other) const noexcept {
+    return StrongType{static_cast<T>(m_Value + other.m_Value)};
+  }
 
-    constexpr StrongType& operator+=(const StrongType& other) noexcept
-    {
-        m_Value += other.m_Value;
-        return *this;
-    }
+  [[nodiscard]] constexpr StrongType operator-(
+      const StrongType &other) const noexcept {
+    return StrongType{static_cast<T>(m_Value - other.m_Value)};
+  }
 
-    constexpr StrongType& operator-=(const StrongType& other) noexcept
-    {
-        m_Value -= other.m_Value;
-        return *this;
-    }
+  constexpr StrongType &operator+=(const StrongType &other) noexcept {
+    m_Value += other.m_Value;
+    return *this;
+  }
 
-    // bitwise
-    [[nodiscard]] constexpr StrongType operator&(const StrongType& other) const noexcept
-    {
-        return StrongType{static_cast<T>(m_Value & other.m_Value)};
-    }
+  constexpr StrongType &operator-=(const StrongType &other) noexcept {
+    m_Value -= other.m_Value;
+    return *this;
+  }
 
-    [[nodiscard]] constexpr StrongType operator|(const StrongType& other) const noexcept
-    {
-        return StrongType{static_cast<T>(m_Value | other.m_Value)};
-    }
+  // bitwise
+  [[nodiscard]] constexpr StrongType operator&(
+      const StrongType &other) const noexcept {
+    return StrongType{static_cast<T>(m_Value & other.m_Value)};
+  }
 
-    [[nodiscard]] constexpr StrongType operator^(const StrongType& other) const noexcept
-    {
-        return StrongType{static_cast<T>(m_Value ^ other.m_Value)};
-    }
+  [[nodiscard]] constexpr StrongType operator|(
+      const StrongType &other) const noexcept {
+    return StrongType{static_cast<T>(m_Value | other.m_Value)};
+  }
 
-    [[nodiscard]] constexpr StrongType operator~() const noexcept
-    {
-        return StrongType{static_cast<T>(~m_Value)};
-    }
+  [[nodiscard]] constexpr StrongType operator^(
+      const StrongType &other) const noexcept {
+    return StrongType{static_cast<T>(m_Value ^ other.m_Value)};
+  }
 
-    [[nodiscard]] constexpr StrongType operator<<(int shift) const noexcept
-    {
-        return StrongType{static_cast<T>(m_Value << shift)};
-    }
+  [[nodiscard]] constexpr StrongType operator~() const noexcept {
+    return StrongType{static_cast<T>(~m_Value)};
+  }
 
-    [[nodiscard]] constexpr StrongType operator>>(int shift) const noexcept
-    {
-        return StrongType{static_cast<T>(m_Value >> shift)};
-    }
+  [[nodiscard]] constexpr StrongType operator<<(int shift) const noexcept {
+    return StrongType{static_cast<T>(m_Value << shift)};
+  }
+
+  [[nodiscard]] constexpr StrongType operator>>(int shift) const noexcept {
+    return StrongType{static_cast<T>(m_Value >> shift)};
+  }
 
 private:
-    T m_Value;
+  T m_Value;
 };
 
 // type tags to differentiate strong types
-namespace tags
-{
-    struct AddressTag
-    {
-    };
+namespace tags {
+struct AddressTag {
+};
 
-    struct OpcodeTag
-    {
-    };
+struct OpcodeTag {
+};
 
-    struct RegisterIndexTag
-    {
-    };
+struct RegisterIndexTag {
+};
 
-    struct RegisterValueTag
-    {
-    };
+struct RegisterValueTag {
+};
 
-    struct KeyTag
-    {
-    };
+struct KeyTag {
+};
 
-    struct PixelTag
-    {
-    };
+struct PixelTag {
+};
 }
 
 // Strong types
@@ -208,71 +193,63 @@ using MemoryView = std::span<const Byte>;
 using MutableMemoryView = std::span<Byte>;
 
 // bit manipulation
-namespace bits
-{
-    //!
-    //! @param value Byte value
-    //! @return extracted high nibble from a byte
-    [[nodiscard]] constexpr Byte high_nibble(Byte value) noexcept
-    {
-        return static_cast<Byte>((value >> 4) & 0x0F);
-    }
+namespace bits {
+//!
+//! @param value Byte value
+//! @return extracted high nibble from a byte
+[[nodiscard]] constexpr Byte high_nibble(Byte value) noexcept {
+  return static_cast<Byte>((value >> 4) & 0x0F);
+}
 
-    //!
-    //! @param value Byte value
-    //! @return extracted low nibble from a byte
-    [[nodiscard]] constexpr Byte low_nibble(Byte value) noexcept
-    {
-        return static_cast<Byte>(value & 0x0F);
-    }
+//!
+//! @param value Byte value
+//! @return extracted low nibble from a byte
+[[nodiscard]] constexpr Byte low_nibble(Byte value) noexcept {
+  return static_cast<Byte>(value & 0x0F);
+}
 
-    //!
-    //! @param high high nibble
-    //! @param low low nibble
-    //! @return combines two bytes into a big-endian word
-    [[nodiscard]] constexpr Word combine(Byte high, Byte low) noexcept
-    {
-        return static_cast<Word>((static_cast<Word>(high) << 8) | low);
-    }
+//!
+//! @param high high nibble
+//! @param low low nibble
+//! @return combines two bytes into a big-endian word
+[[nodiscard]] constexpr Word combine(Byte high, Byte low) noexcept {
+  return static_cast<Word>((static_cast<Word>(high) << 8) | low);
+}
 
-    //! 
-    //! @param value Word value
-    //! @return extracted high byte from a word
-    [[nodiscard]] constexpr Byte high_byte(Word value) noexcept
-    {
-        return static_cast<Byte>((value >> 8) & 0xFF);
-    }
+//!
+//! @param value Word value
+//! @return extracted high byte from a word
+[[nodiscard]] constexpr Byte high_byte(Word value) noexcept {
+  return static_cast<Byte>((value >> 8) & 0xFF);
+}
 
-    //!
-    //! @param value Word value
-    //! @return extracted low byte from a word
-    [[nodiscard]] constexpr Byte low_byte(Word value) noexcept
-    {
-        return static_cast<Byte>(value & 0xFF);
-    }
+//!
+//! @param value Word value
+//! @return extracted low byte from a word
+[[nodiscard]] constexpr Byte low_byte(Word value) noexcept {
+  return static_cast<Byte>(value & 0xFF);
+}
 
-    /// 
-    /// @param value Byte value
-    /// @param bit_position bit position
-    /// @return true/false if the value has a bit set on bit_position
-    [[nodiscard]] constexpr bool is_bit_set(Byte value, int bit_position) noexcept
-    {
-        return (value & (1 << bit_position)) != 0;
-    }
+///
+/// @param value Byte value
+/// @param bit_position bit position
+/// @return true/false if the value has a bit set on bit_position
+[[nodiscard]] constexpr bool is_bit_set(Byte value, int bit_position) noexcept {
+  return (value & (1 << bit_position)) != 0;
+}
 
-    /// 
-    /// @param value Byte
-    /// @return get msb
-    [[nodiscard]] constexpr bool msb(Byte value) noexcept
-    {
-        return static_cast<Byte>((value >> 7) & 0x01);
-    }
+///
+/// @param value Byte
+/// @return get msb
+[[nodiscard]] constexpr bool msb(Byte value) noexcept {
+  return static_cast<Byte>((value >> 7) & 0x01);
+}
 
-    /// 
-    /// @param value Byte
-    /// @return get lsb
-    [[nodiscard]] constexpr bool lsb(Byte value) noexcept
-    {
-        return static_cast<Byte>(value & 0x01);
-    }
+///
+/// @param value Byte
+/// @return get lsb
+[[nodiscard]] constexpr bool lsb(Byte value) noexcept {
+  return static_cast<Byte>(value & 0x01);
+}
+}
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 #include <raylib.h>
 
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
+int main() {
+  std::cout << "Hello, World!" << std::endl;
 
+  InitWindow(800, 600, "Test");
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(BLACK);
+    DrawText("Raylib works!", 300, 280, 20, WHITE);
+    EndDrawing();
+  }
+  CloseWindow();
 
-    InitWindow(800, 600, "Test");
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-        ClearBackground(BLACK);
-        DrawText("Raylib works!", 300, 280, 20, WHITE);
-        EndDrawing();
-    }
-    CloseWindow();
-
-    return 0;
+  return 0;
 }
