07d1fcc821dffe80a5ca2c4dd9ffda4eb23d9568
coa021 <coa2102001@gmail.com>
2026-01-06 20:42:12 +0100

    test(rom_loader): implement ROM loading tests

--- tests/test_rom_loader.cpp
+++ tests/test_rom_loader.cpp
@@ -1,3 +1,19 @@
-//
-// Created by coa on 1/6/2026.
-//
+#include "catch2/catch_test_macros.hpp"
+#include "utils/rom_loader.hpp"
+
+TEST_CASE("RomLoader loads valid ROM file", "[rom]") {
+  std::filesystem::path path{"roms/programs/Chip8 Picture.ch8"};
+  auto result{chip8::RomLoader::load(path)};
+
+  REQUIRE(result.is_ok());
+  REQUIRE(result.value().size() == 164);
+}
+
+
+TEST_CASE("RomLoader loads non existant ROM file", "[rom]") {
+  std::filesystem::path path{"roms/programs/this file does not exist.ch8"};
+  auto result{chip8::RomLoader::load(path)};
+
+  REQUIRE(result.is_err());
+  REQUIRE(result.error().category() == chip8::Error::Category::IO);
+}
