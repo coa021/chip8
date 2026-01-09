a209d6946c8320a8ac9fe58bfea413cd90808794
coa021 <coa2102001@gmail.com>
2026-01-09 20:04:42 +0100

    feat(audio): impl Audio interface

---
+++ include/audio/i_audio.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+
+namespace chip8 {
+
+
+class IAudio {
+public:
+  ~IAudio() = default;
+
+  virtual bool initialize() = 0;
+  virtual void shutdown() = 0;
+  virtual void start_beep() = 0;
+  virtual void stop_beep() = 0;
+
+  virtual bool is_playing() const = 0;
+  virtual void set_frequency(float freq) = 0;
+  virtual void set_volume(float volume) = 0;
+
+protected:
+  IAudio() = default;
+  IAudio(const IAudio &) = delete;
+  IAudio &operator=(const IAudio &) = delete;
+  IAudio(IAudio &&) = delete;
+  IAudio &operator=(IAudio &&) = delete;
+
+
+};
+
+
+}
