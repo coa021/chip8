57356b2aa07b93e7b95d0f39022c61130559b23a
coa021 <coa2102001@gmail.com>
2026-01-10 18:23:56 +0100

    feat(config): impl more options in Config

--- include/utils/config.hpp
+++ include/utils/config.hpp
@@ -6,10 +6,22 @@
 namespace chip8 {
 
 struct Config {
+  int display_scale{12};
+  bool start_fullscreen{false};
+
+  float beep_frequency{440.0f};
+  float beep_volume{0.3f};
+  bool audio_enabled{true};
+
+  double cpu_frequency{500.0};
+  bool shift_quirk{false};
+  bool load_store_quirk{false};
+  bool jump_quirk{false};
+
   bool debug_mode{false};
   LogLevel log_level{LogLevel::Info};
 
-  std::filesystem::path rom_directory{""};
+  std::filesystem::path rom_directory{"./roms"};
   std::filesystem::path last_rom_path{""};
 };
 
